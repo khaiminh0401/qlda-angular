401233a6cabb960eb06c0af22043d500f8b64acc
dkminh.hcm <dkminh.hcm@vnpt.vn>
2024-08-31 00:03:41 +0700

    edit form login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { delay, of, switchMap } from 'rxjs';
 import { AppService } from '../app.service';
+import { ImageService } from '../service/image.service';
+import { AppCommon } from '../app.common';
 
 @Component({
   selector: 'app-login',
@@ -10,16 +11,30 @@ import { AppService } from '../app.service';
   standalone: true,
   imports: [ReactiveFormsModule]
 })
-export class LoginComponent {
+export class LoginComponent implements AppCommon, OnInit {
+  
   loginForm!:FormGroup;
+  public initImg: boolean = false;
+
   constructor(
-    private appService: AppService
-  ){
+    private appService: AppService,
+    public imageService: ImageService
+  ){}
+  ngOnInit(): void {
+    this.setDefault();
+  }
+
+  setDefault(): void {
     this.loginForm = new FormGroup({
       username: new FormControl('', [Validators.required]),
       password: new FormControl('', [Validators.required])
     });
   }
+
+  /**
+   * * Sự kiện nhấn nút đăng nhập
+   * * Call API: auth/login
+   */
   onSubmitForm(){
     const result = this.appService.methodPOST("auth/login", this.loginForm.value);
     console.log(result);
