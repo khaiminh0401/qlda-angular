f09f56c9f6ab8ca4c5ae4d14af96d8905c361fbf
dkminh.hcm <dkminh.hcm@vnpt.vn>
2024-09-01 01:18:03 +0700

    add const

--- src/app/app.const.ts
+++ src/app/app.const.ts
@@ -1,7 +1,11 @@
 export class AppConst {
-    public static page = {
-        login: 'login',
-        home: ''
-    }
-    public static DEFAULT_IMAGE = "assets/common/default.jpg";
+  public static page = {
+    login: 'login',
+    home: '',
+  };
+
+  public static common = {
+    key_token: "token_user"
+  }
+  public static DEFAULT_IMAGE = 'assets/common/default.jpg';
 }
